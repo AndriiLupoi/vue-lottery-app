23b9add50761d72dc0dabb40b5f80d9a22bce283
AndriiLupoi <lupoi_2005@icloud.com>
2024-11-04 23:07:02 +0200

    lab8

--- src/main.js
+++
@@ -1,6 +0,0 @@
-// src/main.ts (or main.js)
-import { createApp } from "vue";
-import App from "./App.vue";
-import "/libs/bootstrap.css";
-createApp(App).mount("#app");
-//# sourceMappingURL=main.js.map
---
+++ src/models/user.ts
@@ -0,0 +1,17 @@
+
+export interface CreateUserRequest {
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+  role: string;
+}
+
+export interface User {
+  id: number;
+  role: string;
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+}
